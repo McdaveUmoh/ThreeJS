3d972934629438b8dac690a7489984e06c619942
demo <demo@gmail.com>
2026-05-11 08:31:33 +0100

    chapter 25

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -3,7 +3,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
-import { toneMapping, toneMappingExposure } from 'three/tsl'
 
 /**
  * Loaders
@@ -30,9 +29,11 @@ const updateAllMaterials = () =>
 {
     scene.traverse((child) =>
     {
-        if(child.isMesh)
+        if(child.isMesh && child.material.isMeshStandardMaterial)
         {
             // Activate shadow here
+            child.castShadow = true
+            child.receiveShadow = true
         }
     })
 }
@@ -61,13 +62,29 @@ rgbeLoader.load('/environmentMaps/0/2k.hdr', (environmentMap) =>
  * Directional Light
  */
 const directionalLight = new THREE.DirectionalLight('#ffffff', 2)
-directionalLight.position.set(3, 7, 6)
+directionalLight.position.set(-4, 6.5, 2.5)
 scene.add(directionalLight)
 
 gui.add(directionalLight, 'intensity', 0, 10).step(0.001).name('lightIntensity')
-gui.add(directionalLight.position, 'x', -10,10).step(0.001).name('lightX')
-gui.add(directionalLight.position, 'y', -10,10).step(0.001).name('lightY')
-gui.add(directionalLight.position, 'z', -10,10).step(0.001).name('lightZ')
+gui.add(directionalLight.position, 'x', -10, 10).step(0.001).name('lightX')
+gui.add(directionalLight.position, 'y', -10, 10).step(0.001).name('lightY')
+gui.add(directionalLight.position, 'z', -10, 10).step(0.001).name('lightZ')
+
+// Shadows
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(512, 512) //the higher the better for qulity but perfomance wise not so
+gui.add(directionalLight, 'castShadow')
+
+// Helper
+// const directionalLightHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(directionalLightHelper)
+
+//Target
+directionalLight.target.position.set(0, 4, 0)
+// scene.add(directionalLight.target)
+directionalLight.target.updateWorldMatrix()
+
 
 /**
  * Models
@@ -129,6 +146,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
 
 // Tone Mapping
 renderer.toneMapping =  THREE.ReinhardToneMapping
